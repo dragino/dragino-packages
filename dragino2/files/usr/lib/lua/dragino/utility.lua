903d7e2d1a3d6076cc78789cfa845193347e4b66
dragino <edwin@dragino.com>
2015-10-30 19:49:44 +0800

    add vendor info

--- dragino2/files/usr/lib/lua/dragino/utility.lua
+++ dragino2/files/usr/lib/lua/dragino/utility.lua
@@ -52,7 +52,7 @@ end
 function getVersion()
 	for line in io.lines('/etc/banner') do 
 		if string.match(line,'Version:[%s]+(.+)') then 
-			f_version = string.match(line,'Version:[%s]+(.+)')  
+			f_version = string.match(line,'Version: Dragino%-v2[%s]+(.+)')  
 		end
 		if string.match(line,'Build[%s]+(.+)') then 
 			b_time = string.match(line,'Build[%s]+(.+)')  

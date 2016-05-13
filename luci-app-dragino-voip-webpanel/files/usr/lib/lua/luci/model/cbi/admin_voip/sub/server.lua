483fd69a4cfef58f3427e56da293777693df592b
dragino <edwin@dragino.com>
2016-05-13 10:32:02 +0800

    add SIP message support

--- luci-app-dragino-voip-webpanel/files/usr/lib/lua/luci/model/cbi/admin_voip/sub/server.lua
+++ luci-app-dragino-voip-webpanel/files/usr/lib/lua/luci/model/cbi/admin_voip/sub/server.lua
@@ -9,6 +9,8 @@ $Id: server_entry.lua 5948 2015-02-10 Dragino Tech $
 ]]--
 
 local uci = luci.model.uci.cursor()
+local util= require('luci.util')
+has_globacom=util.trim(util.exec('cat /etc/banner | grep "globacom" -c'))
 
 m = Map("voip", translate("Configure Server"))
 m.redirect = luci.dispatcher.build_url("admin/voip/servers")
@@ -23,7 +25,7 @@ s = m:section(NamedSection, arg[1], "server", translate("Configure SIP / IAX2 Se
 s.anonymous = true
 s.addremove = false
 
-local sname = s:option(Value, "name", translate("Server Name"),translate("Use for identical locally"))
+--local sname = s:option(Value, "name", translate("Server Name"),translate("Use for identical locally"))
 
 local e_reg = s:option(Flag, "register", translate("Enable Register"),translate("Enable Register to Server"))
 e_reg.enabled  = "1"
@@ -31,8 +33,12 @@ e_reg.disabled = "0"
 e_reg.rmempty  = false
 
 local pro = s:option(ListValue, "protocol", translate("Protocol"))
-pro:value("sip","SIP")
-pro:value("iax2","IAX2")
+pro:value("sip","SIP-General")
+pro:value("iax2","IAX2-General")
+if has_globacom == '1' then
+	pro:value("suissephone","SuissePhone")
+	pro:value("globacom","Globacom")
+end
 
 local sip_host = s:option(Value, "host", translate("Host"))
 sip_host.datatype = "host"
@@ -40,6 +46,8 @@ sip_host.datatype = "host"
 local sip_port = s:option(Value, "port", translate("Register Port"))
 sip_port.datatype = "uinteger"
 sip_port.default = "5060"
+sip_port:depends('protocol','sip')
+sip_port:depends('protocol','iax2')
 
 local fromdomain = s:option(Value, "fromdomain", translate("From Domain"))
 fromdomain.datatype = "host"
@@ -52,6 +60,7 @@ password.password = true
 password.rmempty  = false
 
 local phonenum = s:option(Value, "phonenumber", translate("Phone Number"))
+phonenum:depends('protocol','suissephone')
 
 local fromusername = s:option(Value, "fromusername", translate("From User Name"))
 fromusername:depends('protocol','sip')

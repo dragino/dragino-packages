9ddd12485704a4bf7e6d8119be8076862a667975
root <root@localhost.localdomain>
2018-01-25 17:43:26 +0800

    change gpio port for reset_lgw

--- lora-gateway/src/reset_lgw.sh
+++ lora-gateway/src/reset_lgw.sh
@@ -2,7 +2,7 @@
 
 # This script is intended to be used on IoT Starter Kit platform, it performs
 # the following actions:
-#       - export/unpexort GPIO24 used to reset the SX1301 chip
+#       - export/unpexort GPIO23 used to reset the SX1301 chip
 #
 # Usage examples:
 #       ./reset_lgw.sh stop
@@ -11,7 +11,7 @@
 # The reset pin of SX1301 is wired with RPi GPIO7
 # If used on another platform, the GPIO number can be given as parameter.
 if [ -z "$2" ]; then 
-    IOT_SK_SX1301_RESET_PIN=24
+    IOT_SK_SX1301_RESET_PIN=23
 else
     IOT_SK_SX1301_RESET_PIN=$2
 fi
@@ -22,22 +22,22 @@ WAIT_GPIO() {
 }
 
 iot_sk_init() {
-    # setup GPIO 24
+    # setup GPIO 23
     echo "$IOT_SK_SX1301_RESET_PIN" > /sys/class/gpio/export; WAIT_GPIO
 
-    # set GPIO 24 as output
+    # set GPIO 23 as output
     echo "out" > /sys/class/gpio/gpio$IOT_SK_SX1301_RESET_PIN/direction; WAIT_GPIO
 
     # write output for SX1301 reset
     echo "1" > /sys/class/gpio/gpio$IOT_SK_SX1301_RESET_PIN/value; WAIT_GPIO
     echo "0" > /sys/class/gpio/gpio$IOT_SK_SX1301_RESET_PIN/value; WAIT_GPIO
 
-    # set GPIO 24 as input
+    # set GPIO 23 as input
     echo "in" > /sys/class/gpio/gpio$IOT_SK_SX1301_RESET_PIN/direction; WAIT_GPIO
 }
 
 iot_sk_term() {
-    # cleanup GPIO 24
+    # cleanup GPIO 23
     if [ -d /sys/class/gpio/gpio$IOT_SK_SX1301_RESET_PIN ]
     then
         echo "$IOT_SK_SX1301_RESET_PIN" > /sys/class/gpio/unexport; WAIT_GPIO

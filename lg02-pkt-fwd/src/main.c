0125780d8cdbcefbeb5762ed908e47496f3adfdc
dragino <edwin@dragino.com>
2018-07-16 16:57:08 +0800

    update

--- lg02-pkt-fwd/src/main.c
+++ lg02-pkt-fwd/src/main.c
@@ -188,33 +188,6 @@ void thread_down(void);
 
 /* -------------------------------------------------------------------------- */
 /* --- PRIVATE FUNCTIONS DEFINITION ----------------------------------------- */
-/*
- *  * Print a message and return to caller.
- *   * Caller specifies "errnoflag".
- *    */
-static void err_doit(int errnoflag, int error, const char *fmt, va_list ap)
-{
-    char    buf[MAXLINE];
-
-    vsnprintf(buf, MAXLINE-1, fmt, ap);
-    if (errnoflag)
-        snprintf(buf+strlen(buf), MAXLINE-strlen(buf)-1, ": %s",
-            strerror(error));
-    strcat(buf, "\n");
-    fflush(stdout);     /* in case stdout and stderr are the same */
-    fputs(buf, stderr);
-    fflush(NULL);       /* flushes all stdio output streams */
-}
-
-static void err_quit(const char *fmt, ...)
-{
-    va_list     ap;
-
-    va_start(ap, fmt);
-    err_doit(0, 0, fmt, ap);
-    va_end(ap);
-    exit(1);
-}
 
 static void sig_handler(int sigio) {
 	if (sigio == SIGQUIT) {
@@ -291,115 +264,6 @@ static void pktrx_clean(struct pkt_rx_s *rx) {
     memset(rx->payload, 0, sizeof(rx->payload));
 }
 
-static void daemonize(const char *cmd) {
-	int					i, fd0, fd1, fd2;
-	pid_t				pid;
-	struct rlimit		rl;
-	struct sigaction	sa;
-
-	/*
-	 * Clear file creation mask.
-	 */
-	umask(0);
-
-	/*
-	 * Get maximum number of file descriptors.
-	 */
-	if (getrlimit(RLIMIT_NOFILE, &rl) < 0)
-		err_quit("%s: can't get file limit", cmd);
-
-	/*
-	 * Become a session leader to lose controlling TTY.
-	 */
-	if ((pid = fork()) < 0)
-		err_quit("%s: can't fork", cmd);
-	else if (pid != 0) /* parent */
-		exit(0);
-	setsid();
-
-	/*
-	 * Ensure future opens won't allocate controlling TTYs.
-	 */
-	sa.sa_handler = SIG_IGN;
-	sigemptyset(&sa.sa_mask);
-	sa.sa_flags = 0;
-	if (sigaction(SIGHUP, &sa, NULL) < 0)
-		err_quit("%s: can't ignore SIGHUP", cmd);
-	if ((pid = fork()) < 0)
-		err_quit("%s: can't fork", cmd);
-	else if (pid != 0) /* parent */
-		exit(0);
-
-	/*
-	 * Change the current working directory to the root so
-	 * we won't prevent file systems from being unmounted.
-	 */
-	if (chdir("/") < 0)
-		err_quit("%s: can't change directory to /", cmd);
-
-	/*
-	 * Close all open file descriptors.
-	 */
-	if (rl.rlim_max == RLIM_INFINITY)
-		rl.rlim_max = 1024;
-	for (i = 0; i < rl.rlim_max; i++)
-		close(i);
-
-	/*
-	 * Attach file descriptors 0, 1, and 2 to /dev/null.
-	 */
-	fd0 = open("/dev/null", O_RDWR);
-	fd1 = dup(0);
-	fd2 = dup(0);
-
-	/*
-	 * Initialize the log file.
-	 */
-	openlog(cmd, LOG_CONS, LOG_DAEMON);
-	if (fd0 != 0 || fd1 != 1 || fd2 != 2) {
-		err_quit("%s: unexpected file descriptors %d %d %d",
-		  fd0, fd1, fd2, cmd);
-	}
-}
-
-#define LOCKFILE "/var/run/pkt_fwd.pid"
-#define LOCKMODE (S_IRUSR|S_IWUSR|S_IRGRP|S_IROTH)
-
-static int lockfile(int fd)
-{
-	struct flock fl;
-
-	fl.l_type = F_WRLCK;
-	fl.l_start = 0;
-	fl.l_whence = SEEK_SET;
-	fl.l_len = 0;
-	return(fcntl(fd, F_SETLK, &fl));
-}
-
-static int already_running(void)
-{
-	int		fd;
-	char	buf[16];
-
-	fd = open(LOCKFILE, O_RDWR|O_CREAT, LOCKMODE);
-	if (fd < 0) {
-		syslog(LOG_ERR, "can't open %s: %s", LOCKFILE, strerror(errno));
-		exit(1);
-	}
-	if (lockfile(fd) < 0) {
-		if (errno == EACCES || errno == EAGAIN) {
-			close(fd);
-			return(1);
-		}
-		syslog(LOG_ERR, "can't lock %s: %s", LOCKFILE, strerror(errno));
-		exit(1);
-	}
-	ftruncate(fd, 0);
-	sprintf(buf, "%ld", (long)getpid());
-	write(fd, buf, strlen(buf)+1);
-	return(0);
-}
-
 /* -------------------------------------------------------------------------- */
 /* --- MAIN FUNCTION -------------------------------------------------------- */
 
@@ -450,21 +314,8 @@ int main(int argc, char *argv[])
 	
     unsigned long long ull = 0;
 
-	/* display version informations */
-	//MSG("*** Basic Packet Forwarder for LG02 ***\nVersion: " VERSION_STRING "\n");
-    
-    char *cmd;
-
-    if ((cmd = strrchr(argv[0], '/')) == NULL)
-        cmd = argv[0];
-    else
-        cmd++;
-	
-    daemonize(cmd);  /* daemon server */
-    
-    if (already_running()) {
-        err_quit("%s has already running\n", cmd);
-    }
+    /* display version informations */
+    //MSG("*** Basic Packet Forwarder for LG02 ***\nVersion: " VERSION_STRING "\n");
 
 /* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
 	/* load configuration */

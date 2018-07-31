f1cfeb7fe659e8ffcac2978c6e4de453841daa94
dragino <edwin@dragino.com>
2018-07-31 17:20:20 +0800

    update loragateway

--- luci-app-loragateway/luasrc/model/cbi/admin_gateway/iotserver.lua
+++ luci-app-loragateway/luasrc/model/cbi/admin_gateway/iotserver.lua
@@ -1,18 +1,21 @@
-m = Map("iot-services", translate("Select IoT Service"), translate("Select a IoT Service"))
+local uci = luci.model.uci.cursor()
 
-s = m:section(NamedSection, "general", "iot-services", translate("Select IoT Service"))
+m = Map("iot-services", translate("IoT Service"))
+
+s = m:section(NamedSection, "general", "iot-services")
 local sv = s:option(ListValue, "server_type", translate("IoT Service"))
 sv.placeholder = "Select IoT service"
-sv.default = "mqtt"
-sv:value("lorawan",  "Lorawan/RAW packets forwarder")
+sv.default = "lorawan"
+sv:value("lorawan",  "Lorawan/RAW forwarder")        
 sv:value("relay",  "Lorawan/RAW packets relay")
 sv:value("mqtt",  "LoraRAW forward to MQTT server")
 sv:value("tcpudp",  "LoraRAW forward to TCP/UDP server")
 
-local debug = s:option(ListValue, "logdebug", translate("Debug level"))
-debug.default  = "0"
-debug.value("0", "LEVEL0 only error message output");
-debug.value("1", "LEVEL1 little debug message output");
-debug.value("2", "LEVEL2 more verbose output");
+local lv = s:option(ListValue, "logdebug", translate("Debug Level"))
+lv.placeholder = "Select debug level"
+lv.default = "0"
+lv:value("0",  "No debug")
+lv:value("1",  "Little message output")
+lv:value("2",  "More verbose output")
 
 return m

f5399e90ed3e4761d44ccb8f5f01b3c3c7f379c3
dragino <edwin@dragino.com>
2018-09-26 16:22:49 +0800

    change logread format

--- luci-app-loragateway/luasrc/model/cbi/admin_gateway/iotserver.lua
+++ luci-app-loragateway/luasrc/model/cbi/admin_gateway/iotserver.lua
@@ -8,15 +8,16 @@ sv.placeholder = "Select IoT service"
 sv.default = "disabled"
 sv:value("disabled",  "Disabled") 
 sv:value("lorawan",  "LoRaWan/RAW forwarder")        
-sv:value("relay",  "LoRaWan/RAW packets relay")
-sv:value("mqtt",  "LoRaRAW forward to MQTT server")
-sv:value("tcpudp",  "LoRaRAW forward to TCP/UDP server")
+sv:value("relay",  "LoRaWan/RAW Relay")
+sv:value("mqtt",  "LoRaRAW to MQTT")
+sv:value("tcpudp",  "LoRaRAW to TCP/UDP ")
 
-local lv = s:option(ListValue, "logdebug", translate("Debug Level"))
+local lv = s:option(ListValue, "DEB", translate("Debug Level"))
 lv.placeholder = "Select debug level"
-lv.default = "0"
+lv.default = "1"
 lv:value("0",  "No debug")
 lv:value("1",  "Little message output")
 lv:value("2",  "More verbose output")
+lv:value("3",  "Many verbose output")
 
 return m

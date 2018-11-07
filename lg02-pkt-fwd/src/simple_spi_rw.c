a66801025903fe661e4fa28a5bf56615aa43919e
dragino <edwin@dragino.com>
2018-11-07 15:00:40 +0800

    fix the board detect ulitity

--- lg02-pkt-fwd/src/simple_spi_rw.c
+++ lg02-pkt-fwd/src/simple_spi_rw.c
@@ -1,39 +1,40 @@
 #include "radio.h"
+int DEBUG_INFO = 0;
 
-int DEBUG_INFO = 0;       
-
-int main () {
+int main (int argc, char *argv[]) {
     FILE *fp;
+
+    int rst_pin = 0;
+    int ret = 1;
+
+    if (argc < 3) 
+        return ret;
+
+    rst_pin = atoi(argv[1]);
+
+    if (rst_pin < 0 || rst_pin > 40) 
+        return ret;
 
     radiodev *loradev;
     loradev = (radiodev *) malloc(sizeof(radiodev));
     loradev->nss = 15;
-    loradev->rst = 8;
+    loradev->rst = rst_pin;
     loradev->dio[0] = 7;
     loradev->dio[1] = 6;
     loradev->dio[2] = 0;	
     strcpy(loradev->desc, "LG02 detect");	
-    loradev->spiport = lgw_spi_open(SPI_DEV_RX);
+    loradev->spiport = lgw_spi_open(SPI_DEV_TX);
 
     if ((fp = fopen("/var/iot/board", "w")) < 0) {
         perror("open board");
         close(loradev->spiport);
         free(loradev);
-        return 1;
+        return ret;
     }
 
-    if (get_radio_version(loradev))  
-        fprintf(fp, "LG02\n");
-    else {
-        close(loradev->spiport);
-        loradev->nss = 21;
-        loradev->rst = 12;
-        strcpy(loradev->desc, "LG08P detect");	
-        loradev->spiport = lgw_spi_open(SPI_DEV_TX);
-        if (get_radio_version(loradev))  
-            fprintf(fp, "LG08P\n");
-        else
-            fprintf(fp, "LG08\n");
+    if (get_radio_version(loradev)) {  
+        fprintf(fp, "%s\n", argv[2]);
+        ret = 0;
     }
 
     fflush(fp);
@@ -43,6 +44,6 @@ int main () {
 
     free(loradev);
 
-    return 0;
+    return ret;
 
 }

194ca6821dcded76275e8f89207a9148826f754c
dragino <edwin@dragino.com>
2020-01-08 14:19:00 +0800

    export rssi to file

--- lg02-pkt-fwd/src/lg02_pkt_fwd.c
+++ lg02-pkt-fwd/src/lg02_pkt_fwd.c
@@ -996,10 +996,10 @@ int main(int argc, char *argv[])
                         
                         fp = fopen(chan_path, "w+");
                         if ( NULL != fp ) {
-							clock_gettime(CLOCK_REALTIME, &fetch_time1);
-							x1 = gmtime(&(fetch_time1.tv_sec)); /* split the UNIX timestamp to its calendar components */
-							fprintf(fp,"%04i-%02i-%02iT%02i:%02i:%02i,", (x1->tm_year)+1900, (x1->tm_mon)+1, x1->tm_mday, x1->tm_hour, x1->tm_min, x1->tm_sec); /* ISO 8601 format */
-							fprintf(fp, "%.0f,",pktrx[prev].rssi);
+			    clock_gettime(CLOCK_REALTIME, &fetch_time1);
+		            x1 = gmtime(&(fetch_time1.tv_sec)); /* split the UNIX timestamp to its calendar components */
+                            fprintf(fp,"%04i-%02i-%02iT%02i:%02i:%02i,", (x1->tm_year)+1900, (x1->tm_mon)+1, x1->tm_mday, x1->tm_hour, x1->tm_min, x1->tm_sec); /* ISO 8601 format */
+			    fprintf(fp, "%.0f,",pktrx[pt].rssi);
                             fprintf(fp, "%s\n", chan_data);
                             fflush(fp);
                             fclose(fp);

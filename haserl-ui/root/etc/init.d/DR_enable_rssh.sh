f084a38782519229bfa7f1946def6035aad131ba
mikayong <skerlan@qq.com>
2021-03-25 12:16:40 +0800

    add link form ssh to rssh for rssh connect

--- haserl-ui/root/etc/init.d/DR_enable_rssh.sh
+++ haserl-ui/root/etc/init.d/DR_enable_rssh.sh
@@ -3,6 +3,7 @@ START=99
 
 start()
 {	
+    [ ! -L /usr/bin/rssh ] && ln -sf /usr/bin/ssh /usr/bin/rssh
 	host_id="$(uci -q get rssh.rssh.host_id)"
 	host_addr="$(uci -q get rssh.rssh.host_addr)"
 	host_port="$(uci -q get rssh.rssh.host_port)"
